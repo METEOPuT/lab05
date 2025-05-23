2541637611fc5d9a0e3fc4e91ff98b5edf3275bc
METEOPuT <iklesh32@gmail.com>
2025-05-23 18:46:31 -0400

    Расширено покрытие

--- tests/AccountTest.cpp
+++ tests/AccountTest.cpp
@@ -1,47 +1,31 @@
 #include <gtest/gtest.h>
 #include "MockAccount.h"
 
-TEST(AccountTest, ChangeBalance_IncreasesBalance) {
-    MockAccount acc(1, 0);
+TEST(Account, balance_positive) {
+    Account acc1(0, 1000);
+    EXPECT_EQ(acc1.GetBalance(), 1000);
 
-    EXPECT_CALL(acc, Lock())
-        .Times(1);
-    EXPECT_CALL(acc, ChangeBalance(100))
-        .Times(1);
-    EXPECT_CALL(acc, GetBalance())
-        .Times(1)
-        .WillOnce(::testing::Return(100));
-    EXPECT_CALL(acc, Unlock())
-        .Times(1);
+    acc1.Lock();
+    EXPECT_NO_THROW(acc1.ChangeBalance(100));
 
-    acc.Lock();
-    acc.ChangeBalance(100);
-    EXPECT_EQ(acc.GetBalance(), 100);
-    acc.Unlock();
+    EXPECT_EQ(acc1.GetBalance(), 1100);
 }
 
-TEST(AccountTest, ChangeBalance_DecreasesBalance) {
-    MockAccount acc(1, 100);
+TEST(Accout, balance_negative) {
+    Account Vasya(1, 100);
 
-    EXPECT_CALL(acc, Lock()).Times(1);
-    EXPECT_CALL(acc, ChangeBalance(-30)).Times(1);
-    EXPECT_CALL(acc, GetBalance())
-        .Times(1)
-        .WillOnce(::testing::Return(70));
-    EXPECT_CALL(acc, Unlock()).Times(1);
+    EXPECT_THROW(Vasya.ChangeBalance(100), std::runtime_error);
 
-    acc.Lock();
-    acc.ChangeBalance(-30);
-    EXPECT_EQ(acc.GetBalance(), 70);
-    acc.Unlock();
+    Vasya.Lock();
+    EXPECT_ANY_THROW(Vasya.Lock());
 }
 
-TEST(AccountTest, LockAndUnlockCalled) {
-    MockAccount acc(2, 50);
-
-    EXPECT_CALL(acc, Lock()).Times(1);
-    EXPECT_CALL(acc, Unlock()).Times(1);
-
-    acc.Lock();
-    acc.Unlock();
+TEST(Account, Locker) {
+    MockAccount acc(0, 1111);
+        EXPECT_CALL(acc, Lock()).Times(2);
+        EXPECT_CALL(acc, Unlock()).Times(1);
+        acc.Lock();
+        acc.Lock();
+        acc.Unlock();
 }
+
